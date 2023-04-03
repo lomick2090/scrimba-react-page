d78b62b6a20fb720457dbeedb0bd025131fba294
minkman <lomick2090@gmail.com>
2023-04-03 16:56:00 -0500

    Add full styling besides background

--- react-page/src/components/Main.jsx
+++ react-page/src/components/Main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 
 export default function Main() {
     return (
-        <div>
+        <div className="Main">
             <h1>Fun Facts about React</h1>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
+            <ul className='list'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </div>
     )
