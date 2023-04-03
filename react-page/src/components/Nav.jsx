fb83cbe55801674bbbe8487f4ee8eac5f3cf88eb
minkman <lomick2090@gmail.com>
2023-04-03 16:40:01 -0500

    Finish NavBar

--- react-page/src/components/Nav.jsx
+++ react-page/src/components/Nav.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import icon from '../reactIcon.svg'
 
 export default function Nav() {
-    <div>
-        <img></img>
-        <h3>derp</h3>
-        <h3>derp</h3>
-    </div>
+    return (
+        <div className='Nav'>
+            <div>
+                <img className='icon' src={icon}></img>
+                <h3>ReactFacts</h3>
+            </div>
+            <h4>React Course - Project 1</h4>
+        </div>
+    )
 }
